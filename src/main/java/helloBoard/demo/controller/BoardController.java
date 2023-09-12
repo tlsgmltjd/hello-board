37d4b70507b205b196235dd5837fb2e810eb2e38
신희성 <vvwv3092@gmail.com>
2023-09-12 23:13:16 +0900

    Add comment

--- src/main/java/helloBoard/demo/controller/BoardController.java
+++ src/main/java/helloBoard/demo/controller/BoardController.java
@@ -18,21 +18,27 @@ public class BoardController {
     @Autowired
     BoardService boardService;
 
+    // 게시글 작성하기
     @PostMapping("/post")
     public void saveBoard(@RequestBody BoardCreateRequest boardCreateRequest) { boardService.saveBoard(boardCreateRequest); }
 
+    // 게시글 가져오기
     @GetMapping("/get")
     public List<BoardGetResponse> getBoards() { return boardService.getBoards(); }
 
+    // 게시글 상세 보기
     @GetMapping("/info")
     public BoardInfoResponse getBoard(@RequestParam Long id) { return boardService.getBoard(id); }
 
+    // 게시글 삭제
     @DeleteMapping("/delete")
     public void deleteBoard(@RequestParam Long id) { boardService.deleteBoard(id); }
 
+    // 게시글 수정
     @PutMapping("/update")
     public void updateBoard(@RequestBody BoardUpdateRequest boardUpdateRequest) { boardService.updateBoard(boardUpdateRequest); }
 
+    // 좋아요 기능
     @GetMapping("/like")
     public void likesBoard(@RequestParam Long id) { boardService.likesBoard(id); }
 }
